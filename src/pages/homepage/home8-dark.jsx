2648dd261bf1929cece16598b8af61cac1f55845
J-dev740 <tcr19ec029@gectcr.ac.in>
2022-11-26 18:06:55 +0530

    modified project with minimal settings

--- src/pages/homepage/home8-dark.jsx
+++ src/pages/homepage/home8-dark.jsx
@@ -7,13 +7,13 @@ import Team3 from "../../components/Team3/team3";
 import Intro5 from "../../components/Intro5/intro5";
 import AboutUs6 from "../../components/About-us6/about-us6";
 import Services6 from "../../components/Services6/services6";
-import CaseStudy from "../../components/Case-study/case-study";
+// import CaseStudy from "../../components/Case-study/case-study";
 import NavbarArch from "../../components/Navbar-arch/navbar-arch";
-import LatestNews from "../../components/Latest-news/latest-news";
+// import LatestNews from "../../components/Latest-news/latest-news";
 import WorksStyle5 from "../../components/Works-style5/works-style5";
 import FooterArch from "../../components/Footer-arch/footer-arch.jsx";
 import ContactArch from "../../components/Contact-arch/contact-arch";
-import TestimonialsArch from "../../components/Testimonials-arch/testimonials-arch";
+// import TestimonialsArch from "../../components/Testimonials-arch/testimonials-arch";
 
 const Homepage = () => {
   const navbarRef = React.useRef(null);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Demos from "./demos/demos";
+// import Demos from "./demos/demos";
 import Homepage from "./homepage/home8-dark";
 
 const Home = () => {
